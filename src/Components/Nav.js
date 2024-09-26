3175f492ebb75e8a77626aefed6f585bd9819c83
Akmal1796 <mhdakmal1796@gmail.com>
2024-09-26 15:47:40 +0530

    Completed the basic navigation for the app using react router

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import logo from '..//Images/logo 1.jpg'
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from '../Home';
+import About from '../About';
+import Reservation from '../Reservation';
+import OrderOnline from '../OrderOnline';
+import Login from '../Login';
 
 const Nav = () => {
   return (
     <nav>
         <img src={logo} className='logo'/>
         <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#reserv'>Reservations</a></li>
-            <li><a href='#order'>Order online</a></li>
-            <li><a href='login'>Login</a></li>
+            <li><Link to="/" className='nav-item'>Home Page</Link></li>
+            <li><Link to="/about" className='nav-item'>About</Link></li>
+            <li><Link to="/reservation" className='nav-item'>Reservation</Link></li>
+            <li><Link to="/order" className='nav-item'>Order Online</Link></li>
+            <li><Link to="/login" className='nav-item'>Login</Link></li>
         </ul>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/reservation' element={<Reservation />} />
+            <Route path='/order' element={<OrderOnline />} />
+            <Route path='/login' element={<Login />} />
+        </Routes>
     </nav>
   )
 }
