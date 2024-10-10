c473756632fdc2d2d4526518d47f7cbd9dd5cc00
Akmal1796 <mhdakmal1796@gmail.com>
2024-10-10 16:45:57 +0530

    Completed the Home page banner section

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import heroIMG from './Images/recipe 2.jpg'
+import heroIMG from './Images/recipe 2.jpg';
 
 const Home = () => {
   return (
-    <main>
-      <img src={heroIMG} width={300}/>
+    <main className='homepage-banner'>
+      <div className='banner-text-container'>
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>We are a family owned Mediterranean restaurant. Focused on traditional recipes served with a modern twist.</p>
+        <button className='reserve-btn'>Reserve a Table</button>
+      </div>
+      <div>
+        <img src={heroIMG} className='banner-image'/>
+      </div>
     </main>
   )
 }
