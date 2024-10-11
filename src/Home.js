f51fcfd757876f734ac9baede7066b3bf03d3860
Akmal1796 <mhdakmal1796@gmail.com>
2024-10-11 17:45:53 +0530

    Completed the header section for bigger screen devices

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,17 @@ import heroIMG from './Images/recipe 2.jpg';
 
 const Home = () => {
   return (
-    <main className='homepage-banner'>
-      <div className='banner-text-container'>
-        <h1>Little Lemon</h1>
-        <h3>Chicago</h3>
-        <p>We are a family owned Mediterranean restaurant. Focused on traditional recipes served with a modern twist.</p>
-        <button className='reserve-btn'>Reserve a Table</button>
-      </div>
-      <div>
-        <img src={heroIMG} className='banner-image'/>
+    <main className='home-page'>
+      <div className='homepage-banner'>
+        <div className='banner-text-container'>
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterranean restaurant. Focused on traditional recipes served with a modern twist.</p>
+          <button className='reserve-btn'>Reserve a Table</button>
+        </div>
+        <div className='banner-img-container'>
+          <img src={heroIMG} className='banner-image'/>
+        </div>
       </div>
     </main>
   )
