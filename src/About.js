b77b985960f76f0f2ad07c2ed4aef4dbb0163cd8
Akmal1796 <mhdakmal1796@gmail.com>
2024-10-12 19:28:36 +0530

    Completed the development work on the about page

--- src/About.js
+++ src/About.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import RestauranIMG01 from "./Images/restaurant01.avif";
+import RestauranIMG02 from "./Images/restaurant02.avif";
+import RestauranIMG03 from "./Images/restaurant01.jpg";
+import RestauranIMG04 from "./Images/restaurant02.jpg";
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about-page'>
+      <div className='first-base'>
+        <div className='about-section-text-content'>
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>We are a family-owned Mediterranean restaurant focused on traditional recipes served with a modern twist. Our menu features fresh, locally sourced ingredients, offering vibrant dishes that celebrate the rich flavors of the Mediterranean. Join us for a memorable dining experience!</p>
+        </div>
+        <div className='about-section-image-content'>
+          <img src={RestauranIMG01} alt='Restaurant Image' className='restaurant-img' />
+          <img src={RestauranIMG02} alt='Restaurant Image' className='restaurant-img float-img' />
+        </div>
+      </div>
+      <div className='second-base'>
+        <div className='about-section-image-content'>
+          <img src={RestauranIMG03} alt='Restaurant Image' className='restaurant-img' />
+          <img src={RestauranIMG04} alt='Restaurant Image' className='restaurant-img float-img' />
+        </div>
+        <div className='about-section-text-content'>
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>We are a family-owned Mediterranean restaurant focused on traditional recipes served with a modern twist. Our menu features fresh, locally sourced ingredients, offering vibrant dishes that celebrate the rich flavors of the Mediterranean. Join us for a memorable dining experience!</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
