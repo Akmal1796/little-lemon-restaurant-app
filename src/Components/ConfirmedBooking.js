d833a801752af472d7800d90de8a9a7f1df86de4
Akmal1796 <mhdakmal1796@gmail.com>
2024-10-14 15:32:20 +0530

    Created the confirbooking component

---
+++ src/Components/ConfirmedBooking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmedBooking() {
+  return (
+    <div className="confirmation-page">
+      <h1>Your booking has been confirmed!</h1>
+      <p>Thank you for your reservation. We look forward to serving you!</p>
+    </div>
+  );
+}
+
+export default ConfirmedBooking;
